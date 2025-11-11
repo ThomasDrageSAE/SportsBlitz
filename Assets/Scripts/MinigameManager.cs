be9d16aae14eb802400b196ca86888c1b0060639
Thomas <thomasdrage5@gmail.com>
2025-11-12 01:27:31 +0800

    Created Fencing scene, updated time logic

--- Assets/Scripts/MinigameManager.cs
+++ Assets/Scripts/MinigameManager.cs
@@ -2,13 +2,61 @@ using UnityEngine;
 
 public class MinigameManager : MonoBehaviour
 {
+    [Header("Global Minigame Settings")]
+    public float defaultTimeLimit = 8f;
+
+    private float timer;
+    private bool timerActive = false;
+
+    void Start()
+    {
+        timer = defaultTimeLimit;
+        timerActive = true;
+    }
+
+    void Update()
+    {
+        if (!timerActive) return;
+
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            timerActive = false;
+            TimeUp();
+        }
+    }
+
+    public float GetRemainingTime()
+    {
+        return Mathf.Max(0, timer);
+    }
+
+    public void ResetTimer()
+    {
+        timer = defaultTimeLimit;
+        timerActive = true;
+    }
+
+    public void StopTimer()
+    {
+        timerActive = false;
+    }
+
     public void Win()
     {
+        StopTimer();
         SportsBlitzGameManager.Instance.RegisterWin();
     }
 
     public void Lose()
     {
+        StopTimer();
         SportsBlitzGameManager.Instance.RegisterLoss();
+    }
+
+    private void TimeUp()
+    {
+        Debug.Log("Time's up!");
+        Lose();
     }
 }
