1bd9a53dcbd098dcba0c812907cbdc35ee903d26
Aj-Weston <1033856@student.sae.edu.au>
2025-11-26 20:18:12 +0800

    Popped stash before merge of "origin/Aj" and "Develop"
    Script Code to make ball move.
    Still have to code trigger for when the Ball goes in the Golf Hole.

---
+++ Assets/Scripts/GolfGameScripts/GolfHoleManager.cs
@@ -0,0 +1,16 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GolfHoleManager : MonoBehaviour
+{
+    public BoxCollider2D golfHole;
+
+    /*private void OnTriggerEnter(Collision golfHole)
+    {
+        if (golfHole.transform.GetComponent<golfB
+        {
+            return;
+        }
+    }
+    */
+}
