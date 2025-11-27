177a3742fcf7f0f09c15f4b1221a9dede98c7460
Aj-Weston <1033856@student.sae.edu.au>
2025-11-27 20:22:30 +0800

    Golf Micro Game
    - Added Text to display Win or Lose Condition
    - Added Timer set for 30 seconds
    - Game is mostly complete, going to try and add animations to it before the weekend.

--- Assets/Scripts/GolfGameScripts/GolfHoleManager.cs
+++ Assets/Scripts/GolfGameScripts/GolfHoleManager.cs
@@ -1,16 +1,44 @@
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class GolfHoleManager : MonoBehaviour
 {
     public BoxCollider2D golfHole;
+    public CircleCollider2D golfBall;
+    public TextMeshProUGUI winLoseConditionText;
+    public TextMeshProUGUI timerText;
 
-    /*private void OnTriggerEnter(Collision golfHole)
+    public float time = 30f;
+
+    private void Update()
     {
-        if (golfHole.transform.GetComponent<golfB
+        if(time > 0)
         {
+            time -= Time.deltaTime;
+            timerText.text = "Time: " + time;
+        }
+        else if (time < 0)
+        {
+            time = 0;
+            winLoseConditionText.text = "Times Up, You Lose!!!"; // Message displays when time is up.
+            Debug.Log("Times Up, You Lose!");
+        }
+    }
+
+    public void OnTriggerEnter2D(UnityEngine.Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Goal"))
+        {
+            Debug.Log("Ball is not in Hole");
+        }
+        else
+        {
+            winLoseConditionText.text = "Hole In One, You Win!!!"; // Message will display in console when Ball goes into hole.
+            Debug.Log("Ball has gone into Hole!"); 
             return;
         }
     }
-    */
+
+
 }
